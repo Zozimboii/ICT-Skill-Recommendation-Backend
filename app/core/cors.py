4901b22dda8b1bebc17983e48d65f29fb3b1bfcb
Pruksakorn <pruksakorn147@gmail.com>
2026-03-25 20:13:10 +0700

    FIX:CORS APPROVE VERCEL

--- app/core/cors.py
+++ app/core/cors.py
@@ -4,10 +4,11 @@ from fastapi.middleware.cors import CORSMiddleware
 
 def setup_cors(app):
     origins = [
-        "http://localhost:3000",
-        "http://127.0.0.1:3000",
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
+        # "http://localhost:3000",
+        # "http://127.0.0.1:3000",
+        # "http://localhost:5173",
+        # "http://127.0.0.1:5173",
+        "https://ict-skill-recommendation-frontend.vercel.app",
     ]
     app.add_middleware(
         CORSMiddleware,
